dea60542ea9664745d24616aeac1c305e7e3b6ab
vanner-a <vanner_a@etna-alternance.net>
2017-05-02 17:29:59 +0200

    started api

--- storage/Storage.js
+++ storage/Storage.js
@@ -3,7 +3,7 @@ var cfenv = require("cfenv");
 // load local VCAP configuration  and service credentials
 var vcapLocal;
 try {
-  vcapLocal = require('../../vcap-local.json');
+  vcapLocal = require('../vcap-local.json');
   console.log("storage : Loaded local VCAP", vcapLocal);
 } catch (e) { }
 
@@ -12,21 +12,22 @@ const appEnv = cfenv.getAppEnv(appEnvOpts);
 
 module.exports = class Storage {
 
-  constructor(service, dbName) {
-    if (appEnv.services[service]) { 
+  constructor(service, dbName, callback) {
+   if (appEnv.services[service]) {
         // Load the Cloudant library.
         var Cloudant = require('cloudant');
         // Initialize database with credentials
         var cloudant = Cloudant(appEnv.services[service][0].credentials);
         // Create a new "mydb" database.
         cloudant.db.create(dbName, function(err, data) {
+          callback(cloudant.db.use(dbName));
           if(!err) //err if database doesn't already exists
             console.log("Created database: " + dbName);
         });
         // Specify the database we are going to use (mydb)...
         this.mydb = cloudant.db.use(dbName);
+      }
     }
-  }
 
   // content { "data" : data }
   insert(content) {
--- storage/index.js
+++ storage/index.js
@@ -1,22 +1,54 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var Storage = require('./Storage.js');
-var isAuthenticated = (req) => true; //req.body.password === 'pass';
+const uuidV4 = require('uuid/v4');
 
-var store = new Storage("cloudantNoSQLDB", "mydb");
+var isAuthenticated = (req, callback) => {
+  var token = req.get("Authorization");
+  new Storage("cloudantNoSQLDB", "token", (db) => {
+    db.find({selector:{token}}, (er, result) => {
+      if (result.docs.length <= 0) {
+        callback(false);
+        return;
+      }
+      callback(true);
+    });
+  });
+};
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+var getUserId = (req, callback) => {
+  var token = req.get("Authorization");
+  new Storage("cloudantNoSQLDB", "token", (db) => {
+    db.find({selector:{token}}, (er, result) => {
+      if (result.docs.length <= 0) {
+        callback("");
+        return;
+      }
+      callback(result.docs[0]._id);
+    });
+  });
+}
 
-app.use("/api", (req, res, next) => {
-  if (!isAuthenticated(req)) {
-    res.code(503);
-    res.send("Not authenticated");
-    return;
-  }
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log("cors");
   next();
 });
+
+var tokenRequired = (req, res, next) => {
+  isAuthenticated(req, (bool) => {
+    if (!bool) {
+      res.json({"error": "Not authenticated"});
+      return;
+    }
+    next();
+  });
+};
 
 app.post("/api/v1/add-message", (req, res) => {
   console.log('----------------------------------------', new Date());
@@ -25,7 +57,87 @@ app.post("/api/v1/add-message", (req, res) => {
   res.send("data saved");
 });
 
+app.post("/api/v1/register", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  new Storage("cloudantNoSQLDB", "user", (db) => {
+    db.find({selector:{username}}, (er, result) => {
+      if (result.docs.length > 0)
+        res.json({"error": "User already exist"});
+      else 
+        db.insert({username, password}, (err, data) => {res.json({"res": "Ok"});});
+    });
+  });
+});
+
+app.post("/api/v1/token", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  new Storage("cloudantNoSQLDB", "user", (db) => {
+    db.find({selector:{username, password}}, (er, result) => {
+      if (result.docs.length <= 0)
+        res.json({"error": "User unknown"});
+      else {
+        new Storage("cloudantNoSQLDB", "token", (db) => {
+          var id = result.docs[0]._id;
+          db.find({selector:{_id: id}}, (er, result) => {
+            if (result.docs.length <= 0) {
+              var token = uuidV4();
+              db.insert({_id: id, token}, (err, data) => {
+                res.json({token});
+              });
+            }
+            else
+             res.json({"token": result.docs[0].token}); 
+         });
+        });
+      }
+    });
+  });
+});
+
+app.use("/api/v1/org*", tokenRequired);
+
+app.get("/api/v1/org", (req, res) => {
+  new Storage("cloudantNoSQLDB", "org", (db) => {
+    getUserId(req, (str) => {
+      db.find({selector:{owner: str}}, (er, result) => {
+        res.json(result);
+      });
+    });
+  });
+});
+
+app.post("/api/v1/org", (req, res) => {
+  let name = req.body.name;
+  new Storage("cloudantNoSQLDB", "org", (db) => {
+    getUserId(req, (str) => {
+      db.insert({name, owner: str}, (err, data) => {
+        res.json({"res": "Ok"});
+      });
+    });
+  });
+});
+
+app.get("/api/v1/org/:id", (req, res) => {
+  new Storage("cloudantNoSQLDB", "org", (db) => {
+    getUserId(req, (str) => {
+      db.find({selector:{_id: req.params.id, owner: str}}, (er, result) => {
+        res.json(result);
+      });
+    });
+  });
+});
+
+app.put("/api/v1/org/:id", (req, res) => {
+
+});
+
+app.delete("/api/v1/org/:id", (req, res) => {
+
+});
+
 var port = process.env.PORT || 3001
 app.listen(port, function() {
-    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
