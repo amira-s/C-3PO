63a3ed66e90f8882e3f6d59254afd878956f6ae8
amira-s <amira_s@etna-alternance.net>
2017-05-03 21:31:08 +0200

    superficial watson-services refactor

--- watson-services/index.js
+++ watson-services/index.js
@@ -11,90 +11,97 @@ try {
 let appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 let appEnv = cfenv.getAppEnv(appEnvOpts);
 
+const conversation_service = (() => {
+  const {username, password} = appEnv.services.conversation[0].credentials;
+  return watson.conversation({
+    username,
+    password,
+    version: 'v1',
+    version_date: '2017-04-21'
+  });
+})();
+
+const natural_language_understanding = (() => {
+  const {username, password} = appEnv.services["natural-language-understanding"][0].credentials;
+  const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+  return new NaturalLanguageUnderstandingV1({
+    username,
+    password,
+    'version_date': '2017-02-27'
+  });
+})();
+
+const tone_service = (() => {
+  const {username, password} = appEnv.services["tone_analyzer"][0].credentials;
+  return watson.tone_analyzer({
+    username,
+    password,
+    version: 'v3',
+    version_date: '2016-05-19'
+  });
+});
+
 module.exports = {
-	//msg => string representing user's message
-	conversation : function(msg) {
-		const {username, password} = appEnv.services.conversation[0].credentials;
-		let conv = watson.conversation({
-		  username,
-		  password,
-		  version: 'v1',
-		  version_date: '2017-04-21'
-		});
+  //msg => string representing user's message
+  conversation : function conversation(msg) {
+    return new Promise((resolve, reject) => {
+      conversation_service.message(
+        {
+          workspace_id: '31790861-0e9b-4203-a717-40377000742e',
+          context: msg.context || {},
+          input: msg.input || {}
+        },
+        (err, response) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log('[conversation]: ', JSON.stringify(response, null, 2));
+            resolve(response);
+          }
+        });
+    });
 
-		return new Promise((success, reject) => {
-			conv.message({
-				workspace_id: '31790861-0e9b-4203-a717-40377000742e',
-				context: msg.context || {},
-				input: msg.input || {}
-			}, function(err, response) {
-				if (err) {
-					reject(err);
-				} else {
-					console.log(JSON.stringify(response, null, 2));
-					success(response);
-				}
-			});
-		});
+  },
+  //msg => string representing user's message
+  tone_analyzer : function tone_analyzer(msg) {
+    return new Promise((resolve, reject) => {
+      tone_service.tone({text: msg, tones: "emotion"}, (err, tone) => {
+        if (err)
+          reject(err);
+        else {
+          resolve(tone);
+        }
+      });
+    });
+  },
+  //data is a string of text to analyze = user's input.
+  //maybe accept paramters as a function parameter later
+  //or just the limits number
+  nlu : function nlu(data) {
+    const parameters = {
+      'text': data,
+      'features': {
+        'entities': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 3
+        },
+        'keywords': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 3
+        }
+      }
+    }
 
-	},
-	//msg => string representing user's message
-	tone_analyzer : function(msg) {
-		const {username, password} = appEnv.services["tone_analyzer"][0].credentials;
-
-		var tone = watson.tone_analyzer({
-			username,
-			password,
-			version: 'v3',
-			version_date: '2016-05-19'
-		});
-		return new Promise((success, reject) => {
-			tone.tone({text: msg, tones: "emotion"}, function(err, tone) {	
-			    if (err)
-			      reject(err);
-			    else {
-			      success(tone);
-				}
-			});
-		});
-	},
-	//data is a string of text to analyze = user's input.
-	//maybe accept paramters as a function parameter later
-	//or just the limits number
-	nlu : function(data) {
-		const {username, password} = appEnv.services["natural-language-understanding"][0].credentials;
-		var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
-		var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-		  username,
-		  password,
-		  'version_date': '2017-02-27'
-		});
-	
-		var parameters = {
-		  'text': data,
-		  'features': {
-		    'entities': {
-		      'emotion': true,
-		      'sentiment': true,
-		      'limit': 3
-		    },
-		    'keywords': {
-		      'emotion': true,
-		      'sentiment': true,
-		      'limit': 3
-		    }
-		  }
-		}
-
-		return new Promise((resolve, reject) => {
-			natural_language_understanding.analyze(parameters, function(err, response) {
-			  if (err)
-				reject(err);			  
-			  else
-			  {
-			    resolve(response);
-			  }
-			});
-		});
-	}
+    return new Promise((resolve, reject) => {
+      natural_language_understanding_service.analyze(parameters, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
 }
